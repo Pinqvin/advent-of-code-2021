8b164fcf734d80e4cef3892a57186f8ff5291a1b
Juuso Tapaninen <juuso.tapaninen@gmail.com>
2021-12-02 20:32:05 +0200

    Implement day 1 sliding window solution
    Just use two vectors and simple indexing. Tried to get fancy with
    iterator sums but into_iter() at least seemed to move / consume
    the window vector

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,22 +2,32 @@ use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
 
-fn get_increases(path: &str) -> io::Result<u32> {
+fn get_sliding_window_increases(path: &str) -> io::Result<u32> {
     let file = File::open(path)?;
     let lines = io::BufReader::new(file).lines();
 
+    let mut window = Vec::new();
+    let mut values: Vec<u32> = Vec::new();
     let mut increase_count: u32 = 0;
-    let mut previous_value: u32 = u32::MAX;
 
     for line in lines {
         if let Ok(string) = line {
             let value: u32 = string.parse().unwrap_or_default();
 
-            if value > previous_value {
-                increase_count += 1;
+            window.push(value);
+
+            if window.len() == 3 {
+                values.push(window[0] + window[1] + window[2]);
+                window.remove(0);
             }
 
-            previous_value = value;
+            if values.len() == 2 {
+                if values[1] > values[0] {
+                    increase_count += 1;
+                }
+
+                values.remove(0);
+            }
         }
     }
 
@@ -28,8 +38,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
 
-    println!("Checking increases from file {}", filename);
-    let result = get_increases(filename);
+    println!("Checking sliding window increases from file {}", filename);
+    let result = get_sliding_window_increases(filename);
 
     match result {
         Ok(count) => println!("Found {} increases in values", count),
